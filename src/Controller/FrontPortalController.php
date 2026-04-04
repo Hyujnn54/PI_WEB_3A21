efae7734b3498d1a066e05379a0c9a3f3340ce69
Hyujnn <140203430+Hyujnn54@users.noreply.github.com>
2026-04-04 16:04:24 +0200

    Render front cards dynamically from database records

--- src/Controller/FrontPortalController.php
+++ src/Controller/FrontPortalController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Interview;
+use App\Entity\Job_application;
+use App\Entity\Job_offer;
+use App\Entity\Recruitment_event;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,13 +14,29 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class FrontPortalController extends AbstractController
 {
+    public function __construct(private readonly ManagerRegistry $doctrine)
+    {
+    }
+
     #[Route('/front/job-offers', name: 'front_job_offers')]
     public function jobOffers(Request $request): Response
     {
         $role = (string) $request->query->get('role', 'candidate');
+        $offers = $this->doctrine->getRepository(Job_offer::class)->findBy([], ['id' => 'DESC']);
+
+        $cards = array_map(static function (Job_offer $offer): array {
+            $description = trim((string) $offer->getDescription());
+
+            return [
+                'meta' => sprintf('%s | %s', (string) $offer->getLocation(), (string) $offer->getContract_type()),
+                'title' => (string) $offer->getTitle(),
+                'text' => $description === '' ? 'No description available yet.' : substr($description, 0, 190),
+            ];
+        }, $offers);
 
         return $this->render('front/modules/job_offers.html.twig', [
             'authUser' => ['role' => $role],
+            'cards' => $cards,
         ]);
     }
 
@@ -23,9 +44,22 @@ class FrontPortalController extends AbstractController
     public function jobApplications(Request $request): Response
     {
         $role = (string) $request->query->get('role', 'candidate');
+        $applications = $this->doctrine->getRepository(Job_application::class)->findBy([], ['id' => 'DESC']);
+
+        $cards = array_map(static function (Job_application $application): array {
+            $offer = $application->getOffer_id();
+            $coverLetter = trim((string) $application->getCover_letter());
+
+            return [
+                'meta' => sprintf('Application #%s | %s', (string) $application->getId(), (string) $application->getCurrent_status()),
+                'title' => sprintf('Offer: %s', (string) $offer->getTitle()),
+                'text' => $coverLetter === '' ? 'No cover letter provided.' : substr($coverLetter, 0, 190),
+            ];
+        }, $applications);
 
         return $this->render('front/modules/job_applications.html.twig', [
             'authUser' => ['role' => $role],
+            'cards' => $cards,
         ]);
     }
 
@@ -33,9 +67,21 @@ class FrontPortalController extends AbstractController
     public function events(Request $request): Response
     {
         $role = (string) $request->query->get('role', 'candidate');
+        $events = $this->doctrine->getRepository(Recruitment_event::class)->findBy([], ['id' => 'DESC']);
+
+        $cards = array_map(static function (Recruitment_event $event): array {
+            $description = trim((string) $event->getDescription());
+
+            return [
+                'meta' => sprintf('%s | %s', $event->getEvent_date()->format('d M Y'), (string) $event->getLocation()),
+                'title' => (string) $event->getTitle(),
+                'text' => $description === '' ? 'No event description available yet.' : substr($description, 0, 190),
+            ];
+        }, $events);
 
         return $this->render('front/modules/events.html.twig', [
             'authUser' => ['role' => $role],
+            'cards' => $cards,
         ]);
     }
 
@@ -43,9 +89,23 @@ class FrontPortalController extends AbstractController
     public function interviews(Request $request): Response
     {
         $role = (string) $request->query->get('role', 'candidate');
+        $interviews = $this->doctrine->getRepository(Interview::class)->findBy([], ['id' => 'DESC']);
+
+        $cards = array_map(static function (Interview $interview): array {
+            $application = $interview->getApplication_id();
+            $offer = $application->getOffer_id();
+            $notes = trim((string) $interview->getNotes());
+
+            return [
+                'meta' => sprintf('%s | %s', $interview->getScheduled_at()->format('d M Y | H:i'), (string) $interview->getStatus()),
+                'title' => sprintf('Interview: %s', (string) $offer->getTitle()),
+                'text' => $notes === '' ? 'No interview notes available yet.' : substr($notes, 0, 190),
+            ];
+        }, $interviews);
 
         return $this->render('front/modules/interviews.html.twig', [
             'authUser' => ['role' => $role],
+            'cards' => $cards,
         ]);
     }
 }
